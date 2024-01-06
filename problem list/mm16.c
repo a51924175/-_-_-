5ead1858d34c63b7df7e452963fc9b7d53d1f4f9
a51924175 <136336280+a51924175@users.noreply.github.com>
2024-01-06 18:45:25 +0800

    Update mm16.c

--- problem list/mm16.c
+++ problem list/mm16.c
@@ -3,6 +3,6 @@ int main(){
     int x,y;
     scanf("%d %d",&x,&y);
     int dis=(x*x)+(y*y);
-    if(dis<=40000) printf("inside\n");
+    if(dis<=10000) printf("inside\n");
     else printf("outside\n");
 }
